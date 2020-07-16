8846b6b916e00bb9343bbfee3c4624e9358b0ffd
Martín Ciparelli <mciparelli@gmail.com>
2020-07-16 14:45:05 -0300

    fix: HTML base url for js fetch scripts

--- config/generateHtml.js
+++ config/generateHtml.js
@@ -2,11 +2,14 @@ module.exports = ({ appName, appUrl }) => `<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8" />
     <title>demo app</title>
-    <base href="${appUrl}">
     <link rel="stylesheet" href="${appName}.css" />
   </head>
   <body>
     <div id="app"></div>
+    <script>window.highpoint = {dataURI: "${appUrl}".replace(
+      /IScript_.*/,
+      'IScript_'
+    )}</script>
     <script type="text/javascript" src="${appName}_vendor.js"></script>
     <script type="text/javascript" src="${appName}_app.js"></script>
   </body>
