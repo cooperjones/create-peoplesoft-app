143ee81fc3caed4dfa64885c8f6fcfb77c0201dd
Martín Ciparelli <mciparelli@gmail.com>
2020-07-16 13:54:56 -0300

    fix: call initial data on mount

--- config/generateHtml.js
+++ config/generateHtml.js
@@ -1,7 +1,8 @@
-module.exports = appName => `<html lang="en" dir="ltr">
+module.exports = ({ appName, appUrl }) => `<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8" />
     <title>demo app</title>
+    <base href="${appUrl}">
     <link rel="stylesheet" href="${appName}.css" />
   </head>
   <body>
--- config/index.js
+++ config/index.js
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
+import { json } from "@highpoint/js-fetch";
 
-render(<div>hello world!</div>, document.getElementById("app"));
+const App = () => {
+  const [initialData, setInitialData] = useState();
+  useEffect(() => {
+    json("InitialData").then(newData => setInitialData(newData));
+  }, []);
+  return (
+    <>
+      <div>hello world!</div>
+      {initialData && (
+        <div>initial data: {JSON.stringify(initialData, null, 2)}</div>
+      )}
+    </>
+  );
+};
+
+render(<App />, document.getElementById("app"));
